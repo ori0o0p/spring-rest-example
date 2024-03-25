fa308fb8c96d8765a6eb72fb06ca40005fcac07d
101 <tmddnjs001133@dsm.hs.kr>
2024-03-25 14:52:12 +0900

    project start🔥

---
+++ rest-domain/src/main/java/rest/project/SpringRestExampleApplication.java
@@ -0,0 +1,13 @@
+package rest.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestExampleApplication.class, args);
+    }
+
+}
