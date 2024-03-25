d2932ed2fd2f221f36e356dfbf0ee82e4e255d33
101 <tmddnjs001133@dsm.hs.kr>
2024-03-25 16:23:27 +0900

    add :: ArticleAdapter.java

---
+++ rest-domain/src/main/java/rest/project/domain/article/adapter/ArticleAdapter.java
@@ -0,0 +1,47 @@
+package rest.project.domain.article.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import rest.project.domain.article.dao.ArticleRepository;
+import rest.project.domain.article.model.Article;
+import rest.project.domain.article.port.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleAdapter implements FindArticlePort, SaveArticlePort, DeleteArticlePort {
+    private final ArticleRepository articleRepository;
+
+    @Override
+    public void deleteById(Long id) {
+        articleRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Article article) {
+        articleRepository.delete(article);
+    }
+
+    @Override
+    public Optional<Article> findById(Long id) {
+        return articleRepository.findById(id);
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return articleRepository.findAll();
+    }
+
+    @Override
+    public List<Article> findAllByTextContaining(String text) {
+        return articleRepository.findAllByTextContaining(text);
+    }
+
+    @Override
+    public Article save(Article article) {
+        return articleRepository.save(article);
+    }
+
+}
