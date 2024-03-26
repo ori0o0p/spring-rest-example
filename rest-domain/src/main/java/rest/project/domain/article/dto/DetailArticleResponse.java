4c68c7ca7150d263f724453a1dcd4fef7c15b838
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 10:20:38 +0900

    update :: DetailArticleResponse.java
    add relation annotation

--- rest-domain/src/main/java/rest/project/domain/article/dto/DetailArticleResponse.java
+++ rest-domain/src/main/java/rest/project/domain/article/dto/DetailArticleResponse.java
@@ -1,9 +1,11 @@
 package rest.project.domain.article.dto;
 
 import lombok.Builder;
+import org.springframework.hateoas.server.core.Relation;
 import rest.project.domain.article.model.Article;
 
 @Builder
+@Relation(collectionRelation = "data")
 public record DetailArticleResponse(
         Long id,
         String title,
