a23e437dd41a941bc1deb061c446720f8256d1ad
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 23:16:17 +0900

    update :: DeleteArticleService.java

--- rest-service/src/main/java/rest/project/domain/article/service/DeleteArticleService.java
+++ rest-service/src/main/java/rest/project/domain/article/service/DeleteArticleService.java
@@ -2,10 +2,12 @@ package rest.project.domain.article.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rest.project.domain.article.port.DeleteArticlePort;
 import rest.project.domain.article.usecase.DeleteArticleUseCase;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class DeleteArticleService implements DeleteArticleUseCase {
     private final DeleteArticlePort deleteArticlePort;
