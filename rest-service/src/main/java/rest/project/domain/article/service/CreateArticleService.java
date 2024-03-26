6af264fb86e6b0f55acb2341c9ea3417f5e758bd
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 23:16:04 +0900

    update :: CreateArticleService.java

--- rest-service/src/main/java/rest/project/domain/article/service/CreateArticleService.java
+++ rest-service/src/main/java/rest/project/domain/article/service/CreateArticleService.java
@@ -2,11 +2,13 @@ package rest.project.domain.article.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rest.project.domain.article.dto.CreateArticleRequest;
 import rest.project.domain.article.port.SaveArticlePort;
 import rest.project.domain.article.usecase.CreateArticleUseCase;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CreateArticleService implements CreateArticleUseCase {
     private final SaveArticlePort saveArticlePort;
