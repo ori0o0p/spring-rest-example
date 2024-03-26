c3d0ee4e2f0ed965e67fc407c24effa0d758cf0c
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 22:00:14 +0900

    update :: CommentController.java

--- rest-api/src/main/java/rest/project/domain/comment/controller/CommentController.java
+++ rest-api/src/main/java/rest/project/domain/comment/controller/CommentController.java
@@ -4,16 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import rest.project.domain.comment.dto.CreateCommentRequest;
 import rest.project.domain.comment.usecase.CreateCommentUseCase;
+import rest.project.domain.comment.usecase.DeleteCommentUseCase;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/articles/{articleId}/comments")
 public class CommentController {
     private final CreateCommentUseCase createCommentUseCase;
+    private final DeleteCommentUseCase deleteCommentUseCase;
 
     @PostMapping
     public void create(@RequestBody CreateCommentRequest request, @PathVariable Long articleId) {
         createCommentUseCase.create(request, articleId);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public void delete(@PathVariable String articleId, @PathVariable Long commentId) {
+        deleteCommentUseCase.deleteById(commentId);
     }
 
 }
