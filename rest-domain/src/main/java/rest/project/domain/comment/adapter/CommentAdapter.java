2becf0397f3aacea30853cccaa59e6f3d9978680
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 11:39:23 +0900

    add :: CommentAdapter.java

---
+++ rest-domain/src/main/java/rest/project/domain/comment/adapter/CommentAdapter.java
@@ -0,0 +1,33 @@
+package rest.project.domain.comment.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import rest.project.domain.comment.dao.CommentRepository;
+import rest.project.domain.comment.model.Comment;
+import rest.project.domain.comment.port.SaveCommentPort;
+import rest.project.domain.comment.port.DeleteCommentPort;
+import rest.project.domain.comment.port.FindCommentPort;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CommentAdapter implements SaveCommentPort, DeleteCommentPort, FindCommentPort {
+    private final CommentRepository commentRepository;
+
+    @Override
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        commentRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Comment> findAllByArticle(Long articleId) {
+        return commentRepository.findAllByArticle_IdOrderByCreatedAtDesc(articleId);
+    }
+
+}
