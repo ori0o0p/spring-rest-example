549653a10d23fe929541cba1d43fcfa16c863e7b
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 11:38:02 +0900

    update :: DetailArticleResponse.java

--- rest-domain/src/main/java/rest/project/domain/article/dto/DetailArticleResponse.java
+++ rest-domain/src/main/java/rest/project/domain/article/dto/DetailArticleResponse.java
@@ -3,21 +3,31 @@ package rest.project.domain.article.dto;
 import lombok.Builder;
 import org.springframework.hateoas.server.core.Relation;
 import rest.project.domain.article.model.Article;
+import rest.project.domain.comment.model.Comment;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @Relation(collectionRelation = "data")
 public record DetailArticleResponse(
         Long id,
         String title,
-        String content
+        String content,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        List<Comment> comments
         /* other.. */
 ) {
 
-    public static DetailArticleResponse from(Article article) {
+    public static DetailArticleResponse from(Article article, List<Comment> comments) {
         return DetailArticleResponse.builder()
                 .id(article.getId())
                 .title(article.getTitle())
                 .content(article.getContent())
+                .createdAt(article.getCreatedAt())
+                .updatedAt(article.getUpdatedAt())
+                .comments(comments)
                 .build();
     }
 
