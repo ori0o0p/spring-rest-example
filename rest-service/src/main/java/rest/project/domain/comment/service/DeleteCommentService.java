d8302cef6073f3ccd2a33f03d54e2b1ea2a3dfb2
101 <tmddnjs001133@dsm.hs.kr>
2024-03-26 23:16:44 +0900

    update :: DeleteCommentService.java

--- rest-service/src/main/java/rest/project/domain/comment/service/DeleteCommentService.java
+++ rest-service/src/main/java/rest/project/domain/comment/service/DeleteCommentService.java
@@ -2,10 +2,12 @@ package rest.project.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rest.project.domain.comment.port.DeleteCommentPort;
 import rest.project.domain.comment.usecase.DeleteCommentUseCase;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class DeleteCommentService implements DeleteCommentUseCase {
     private final DeleteCommentPort deleteCommentPort;
