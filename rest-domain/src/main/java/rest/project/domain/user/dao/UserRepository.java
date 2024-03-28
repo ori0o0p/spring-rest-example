518b7fd006f6fc1a12f7996843a7db959f9abe94
101 <tmddnjs001133@dsm.hs.kr>
2024-03-29 00:25:35 +0900

    update :: UserRepository.java

--- rest-domain/src/main/java/rest/project/domain/user/dao/UserRepository.java
+++ rest-domain/src/main/java/rest/project/domain/user/dao/UserRepository.java
@@ -3,7 +3,10 @@ package rest.project.domain.user.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rest.project.domain.user.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
