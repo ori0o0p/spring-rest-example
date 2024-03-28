e3fba408ab7bc9f465a9e4b2c2d97e5520e1b722
101 <tmddnjs001133@dsm.hs.kr>
2024-03-28 12:03:06 +0900

    update :: CommentController.java

--- rest-api/src/main/java/rest/project/domain/comment/controller/CommentController.java
+++ rest-api/src/main/java/rest/project/domain/comment/controller/CommentController.java
@@ -1,10 +1,16 @@
 package rest.project.domain.comment.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import rest.project.domain.comment.dao.CommentRepository;
 import rest.project.domain.comment.dto.CreateCommentRequest;
+import rest.project.domain.comment.model.Comment;
+import rest.project.domain.comment.port.FindCommentPort;
 import rest.project.domain.comment.usecase.CreateCommentUseCase;
 import rest.project.domain.comment.usecase.DeleteCommentUseCase;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,11 +20,13 @@ public class CommentController {
     private final DeleteCommentUseCase deleteCommentUseCase;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody CreateCommentRequest request, @PathVariable Long articleId) {
         createCommentUseCase.create(request, articleId);
     }
 
     @DeleteMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String articleId, @PathVariable Long commentId) {
         deleteCommentUseCase.deleteById(commentId);
     }
