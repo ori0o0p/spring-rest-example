d89491c5da60a8a0d5934c54c3ec73cee53c61cc
101 <tmddnjs001133@dsm.hs.kr>
2024-03-29 00:24:15 +0900

    refactor :: ArticleRepository.java

--- rest-domain/src/main/java/rest/project/domain/article/dao/ArticleRepository.java
+++ rest-domain/src/main/java/rest/project/domain/article/dao/ArticleRepository.java
@@ -1,11 +1,7 @@
 package rest.project.domain.article.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import rest.project.domain.article.model.Article;
 
-import java.util.List;
-
 public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleCustomRepository {
-
 }
