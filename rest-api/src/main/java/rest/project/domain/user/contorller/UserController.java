69fc7363818f6e62bfd560b4a339f03033afe1ea
101 <tmddnjs001133@dsm.hs.kr>
2024-03-29 00:28:32 +0900

    update :: UserController.java

--- rest-api/src/main/java/rest/project/domain/user/contorller/UserController.java
+++ rest-api/src/main/java/rest/project/domain/user/contorller/UserController.java
@@ -3,7 +3,10 @@ package rest.project.domain.user.contorller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import rest.project.domain.token.usecase.CreateTokenUseCase;
 import rest.project.domain.user.dto.CreateUserRequest;
+import rest.project.domain.user.dto.UserLoginRequest;
+import rest.project.domain.user.dto.UserLoginResponse;
 import rest.project.domain.user.usecase.CreateUserUseCase;
 
 @RestController
@@ -11,11 +14,17 @@ import rest.project.domain.user.usecase.CreateUserUseCase;
 @RequestMapping("/api/users")
 public class UserController {
     private final CreateUserUseCase createUserUseCase;
+    private final CreateTokenUseCase createTokenUseCase;
 
-    @PostMapping
+    @PostMapping("/auth/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public void signup(@RequestBody CreateUserRequest request) {
         createUserUseCase.save(request);
+    }
+
+    @PostMapping("/auth/login")
+    public UserLoginResponse login(@RequestBody UserLoginRequest request) {
+        return createTokenUseCase.execute(request);
     }
 
 }
