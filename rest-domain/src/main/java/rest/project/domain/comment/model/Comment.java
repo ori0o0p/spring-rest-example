513e35e8c956ecb20f7aa90f4f626ef582acd40e
101 <tmddnjs001133@dsm.hs.kr>
2024-03-28 11:01:18 +0900

    refactor :: Comment.java

--- rest-domain/src/main/java/rest/project/domain/comment/model/Comment.java
+++ rest-domain/src/main/java/rest/project/domain/comment/model/Comment.java
@@ -1,9 +1,11 @@
 package rest.project.domain.comment.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 import rest.project.domain.article.model.Article;
 
 import java.time.LocalDateTime;
@@ -16,13 +18,15 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 500, nullable = false)
     private String content;
 
     @ManyToOne
-    @JsonIgnore
+    @JsonBackReference
     @JoinColumn(name = "article_id")
     private Article article;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
     protected Comment() {
@@ -32,7 +36,6 @@ public class Comment {
     public Comment(String content, Article article) {
         this.content = content;
         this.article = article;
-        this.createdAt = LocalDateTime.now();
     }
 
 }
