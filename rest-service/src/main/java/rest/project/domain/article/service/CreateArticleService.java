13e466e263937dcc34483c8df3e7bb94d48ab367
101 <tmddnjs001133@dsm.hs.kr>
2024-03-29 00:27:24 +0900

    update :: CreateArticleService.java

--- rest-service/src/main/java/rest/project/domain/article/service/CreateArticleService.java
+++ rest-service/src/main/java/rest/project/domain/article/service/CreateArticleService.java
@@ -6,17 +6,21 @@ import org.springframework.transaction.annotation.Transactional;
 import rest.project.domain.article.dto.CreateArticleRequest;
 import rest.project.domain.article.port.SaveArticlePort;
 import rest.project.domain.article.usecase.CreateArticleUseCase;
+import rest.project.domain.user.service.CurrentUser;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CreateArticleService implements CreateArticleUseCase {
     private final SaveArticlePort saveArticlePort;
+    private final CurrentUser currentUser;
 
     @Override
     public void create(CreateArticleRequest request) {
         saveArticlePort.save(
-                CreateArticleRequest.toArticleEntity(request)
+                CreateArticleRequest.toArticleEntity(request,
+                        currentUser.execute()
+                )
         );
     }
 
