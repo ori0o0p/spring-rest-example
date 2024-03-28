754adf6ff3dfac201fbcb7e63043a136895f8ac4
101 <tmddnjs001133@dsm.hs.kr>
2024-03-28 12:05:22 +0900

    update :: UserController.java

--- rest-api/src/main/java/rest/project/domain/user/contorller/UserController.java
+++ rest-api/src/main/java/rest/project/domain/user/contorller/UserController.java
@@ -2,15 +2,13 @@ package rest.project.domain.user.contorller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rest.project.domain.user.dto.CreateUserRequest;
 import rest.project.domain.user.usecase.CreateUserUseCase;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
     private final CreateUserUseCase createUserUseCase;
 
