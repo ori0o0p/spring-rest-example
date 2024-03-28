67d5b41082e60e844112fd52fb026ae8a0ce02e0
101 <tmddnjs001133@dsm.hs.kr>
2024-03-29 00:23:37 +0900

    refactor :: Tokenizer

--- rest-auth/src/main/java/rest/project/Tokenizer.java
+++ rest-auth/src/main/java/rest/project/Tokenizer.java
@@ -1,50 +1,10 @@
 package rest.project;
 
-import io.jsonwebtoken.*;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.Collections;
-
-@Component
-public class Tokenizer {
-
-    public String generateToken(String email) {
-        return Jwts.builder()
-                .setIssuedAt(Date.valueOf(LocalDate.now()))
-                .setSubject(email)
-                .signWith(SignatureAlgorithm.HS512, "rest-project")
-                .setExpiration(Date.valueOf(LocalDate.now().plusDays(1)))
-                .compact();
-    }
-
-    public Authentication getAuthentication(String token) {
-        UserDetails userDetails = createUserDetails(token);
-        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-    }
-
-    private UserDetails createUserDetails(String token) {
-        String subject = Jwts.parser()
-                .setSigningKey("rest-project")
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-
-        return new User(subject, "", Collections.emptyList());
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser().setSigningKey("rest-project").parseClaimsJws(token);
-            return true;
-        } catch (JwtException e) {
-            return false;
-        }
-    }
+public interface Tokenizer {
+    String generateToken(String email);
+    Authentication getAuthentication(String token);
+    boolean validateToken(String token);
 
 }
