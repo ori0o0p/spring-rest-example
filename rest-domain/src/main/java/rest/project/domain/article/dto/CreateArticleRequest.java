3a1e7444eb873db7e5841c8c920d2b5c9958cf2f
101 <tmddnjs001133@dsm.hs.kr>
2024-03-29 00:26:25 +0900

    update :: CreateArticleRequest.java

--- rest-domain/src/main/java/rest/project/domain/article/dto/CreateArticleRequest.java
+++ rest-domain/src/main/java/rest/project/domain/article/dto/CreateArticleRequest.java
@@ -1,16 +1,18 @@
 package rest.project.domain.article.dto;
 
 import rest.project.domain.article.model.Article;
+import rest.project.domain.user.model.User;
 
 public record CreateArticleRequest(
         String title,
         String content
 ) {
 
-    public static Article toArticleEntity(CreateArticleRequest request) {
+    public static Article toArticleEntity(CreateArticleRequest request, User user) {
         return Article.builder()
                 .title(request.title)
                 .content(request.content)
+                .user(user)
                 .build();
     }
 
