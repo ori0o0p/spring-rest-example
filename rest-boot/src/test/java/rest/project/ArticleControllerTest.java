935ab8fd201d86b7c40638ba6e4401b3ed94ff99
101 <tmddnjs001133@dsm.hs.kr>
2024-03-28 13:25:47 +0900

    refactor :: ArticleControllerTest.java

--- rest-boot/src/test/java/rest/project/ArticleControllerTest.java
+++ rest-boot/src/test/java/rest/project/ArticleControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import rest.project.domain.article.controller.ArticleController;
 import rest.project.domain.article.dto.CreateArticleRequest;
