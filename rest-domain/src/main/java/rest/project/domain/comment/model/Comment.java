1d56de471eb73b769c4db1868aa30872b21362c8
101 <tmddnjs001133@dsm.hs.kr>
2024-03-28 16:03:34 +0900

    update :: Comment.java

--- rest-domain/src/main/java/rest/project/domain/comment/model/Comment.java
+++ rest-domain/src/main/java/rest/project/domain/comment/model/Comment.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import rest.project.domain.article.model.Article;
+import rest.project.domain.user.model.User;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @Entity(name = "comment")
 @Table(name = "comment")
 public class Comment {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,21 +23,26 @@ public class Comment {
     @Column(length = 500, nullable = false)
     private String content;
 
-    @ManyToOne
-    @JsonBackReference
-    @JoinColumn(name = "article_id")
-    private Article article;
-
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @JsonBackReference
+    @JoinColumn(name = "article_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Article article;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private User user;
 
     protected Comment() {
     }
 
     @Builder
-    public Comment(String content, Article article) {
+    public Comment(String content, Article article, User user) {
         this.content = content;
         this.article = article;
+        this.user = user;
     }
 
 }
