9f2d689cb77a064e9d6df3557dac70077662ecfe
101 <tmddnjs001133@dsm.hs.kr>
2024-03-30 21:41:57 +0900

    add :: TagMapAdapter.java

---
+++ rest-domain/src/main/java/rest/project/domain/tag/adapter/tagmap/TagMapAdapter.java
@@ -0,0 +1,38 @@
+package rest.project.domain.tag.adapter.tagmap;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import rest.project.domain.tag.model.tagmap.TagMap;
+import rest.project.domain.tag.port.tagmap.FindTagMapPort;
+import rest.project.domain.tag.port.tagmap.SaveTagMapPort;
+import rest.project.domain.tag.repository.tagmap.TagMapRepository;
+
+import java.util.List;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class TagMapAdapter implements SaveTagMapPort, FindTagMapPort {
+    private final TagMapRepository tagMapRepository;
+
+    @Override
+    public TagMap save(TagMap tagMap) {
+        return tagMapRepository.save(tagMap);
+    }
+
+    @Override
+    public void saveAll(Set<TagMap> tagMaps) {
+        tagMapRepository.saveAll(tagMaps);
+    }
+
+    @Override
+    public List<TagMap> findAllByArticleId(Long articleId) {
+        return tagMapRepository.findAllByArticle_IdOrderByTagAsc(articleId);
+    }
+
+    @Override
+    public List<TagMap> findAll() {
+        return tagMapRepository.findAll();
+    }
+
+}
