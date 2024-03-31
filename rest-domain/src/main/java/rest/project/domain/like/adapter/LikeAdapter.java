8454a67020e1c4cd3fc56922e816f2c7110dc9a4
101 <tmddnjs001133@dsm.hs.kr>
2024-03-31 16:05:24 +0900

    add :: LikeAdapter.java

---
+++ rest-domain/src/main/java/rest/project/domain/like/adapter/LikeAdapter.java
@@ -0,0 +1,31 @@
+package rest.project.domain.like.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import rest.project.domain.like.model.Like;
+import rest.project.domain.like.port.DeleteLikePort;
+import rest.project.domain.like.port.FindLikePort;
+import rest.project.domain.like.port.SaveLikePort;
+import rest.project.domain.like.repository.LikeRepository;
+
+@Component
+@RequiredArgsConstructor
+public class LikeAdapter implements SaveLikePort, DeleteLikePort, FindLikePort {
+    private final LikeRepository likeRepository;
+
+    @Override
+    public void deleteByArticleId(Long userId, Long articleId) {
+        likeRepository.deleteByUser_IdAndArticle_Id(userId, articleId);
+    }
+
+    @Override
+    public boolean isLikeCurrentUser(Long userId, Long articleId) {
+        return likeRepository.existsByUser_IdAndArticle_Id(userId, articleId);
+    }
+
+    @Override
+    public Like save(Like like) {
+        return likeRepository.save(like);
+    }
+
+}
