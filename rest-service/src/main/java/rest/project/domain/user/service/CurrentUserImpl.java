d67b79e9a7b5ecf28fe6f43708c808ba9e69c04e
101 <tmddnjs001133@dsm.hs.kr>
2024-04-03 09:46:36 +0900

    update :: CurrentUserImpl.java

--- rest-service/src/main/java/rest/project/domain/user/service/CurrentUserImpl.java
+++ rest-service/src/main/java/rest/project/domain/user/service/CurrentUserImpl.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import rest.project.domain.user.model.User;
 import rest.project.domain.user.port.FindUserPort;
 
 @Component
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CurrentUserImpl implements CurrentUser {
     private final FindUserPort findUserPort;
